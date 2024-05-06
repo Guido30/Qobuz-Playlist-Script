f8d372f727c9f5a084f4d7bcf789a64f590e6ce1
Guido30 <guidoguglietta08@gmail.com>
2024-05-06 20:18:15 +0200

    Now works with updated qobuz login page, Removed downloading since doubledouble implemented captcha and its not ethical

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # Qobuz Playlist Script
 
-Get all track urls from a given qobuz playlist, also able to download using slavartdl
+This simple script retrieves all track urls in a given qobuz playlist
 
-Only supports play.qobuz.com domain and you must have a qobuz account  
-Only supports firefox for now, because thats the browser i use :)
+Only supports play.qobuz.com links and you must provide a qobuz account  
+Only supports firefox because im lazy :)
 
 ## Usage
 
 - Download and install [python3](https://www.python.org/downloads/) and [firefox](https://www.mozilla.org/en-US/firefox/new/)
 - Install packages using `pip install -r requirements.txt`
-- (Optional) To be able to download the songs you must download [slavartdl](https://github.com/tywil04/slavartdl)
 - Rename config.yml.sample to config.yml and configure it
 - Run the script `python main.py`
-
-You can find all the collected tracks urls in the output/tracks.txt file  
-Some tracks might fail to download due to geo restrictions or other issues on qobuz/slavart end
+- You can find all the collected urls in the output/tracks.txt file
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
 import sys
 import os
 import re
@@ -53,144 +52,90 @@ def main():
 
     if not os.path.exists("output"):
         os.makedirs("output")
-    if not os.path.exists("output/tracks"):
-        os.makedirs("output/tracks")
 
-    # Ask user how to proceed
-    print("1. Retrieve Urls from Playlist")
-    print("2. Load Urls and Download")
-    choice = input(": ")
+    email = config["email"]
+    password = config["password"]
 
-    if choice == "1":
+    # Ask the user for the URL
+    url = input("Qobuz Playlist URL: ")
 
-        email = config["email"]
-        password = config["password"]
+    if "open" in url:
+        print("Url open.qobuz.com not supported!\nUse play.qobuz.com instead!")
+        driver.quit()
+        sys.exit(0)
 
-        # Ask the user for the URL
-        url = input("Qobuz Playlist URL: ")
+    driver.get(url)
+    print("Login")
 
-        if "open" in url:
-            print("Url open.qobuz.com not supported!\nUse play.qobuz.com instead!")
-            driver.quit()
-            sys.exit(0)
+    cookies_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[id="onetrust-accept-btn-handler"]')))
+    cookies_button.click()
 
-        driver.get(url)
-        print("Login")
+    time.sleep(2)
 
-        # Find <input name="login"> and insert the email
-        cookies_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[id="onetrust-accept-btn-handler"]')))
-        cookies_button.click()
+    login_btn = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.ButtonPrimary')))
+    login_btn.click()
 
-        # Find <input name="login"> and insert the email
-        login_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="login"]')))
-        login_input.send_keys(email)
+    login_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="_username"]')))
+    login_input.send_keys(email)
 
-        password_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="password"]')))
-        password_input.send_keys(password)
+    password_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="_password"]')))
+    password_input.send_keys(password)
 
-        hide_element_script = """
-        var element = document.querySelector('div.onetrust-pc-dark-filter.ot-fade-in');
-        if (element) element.style.display = 'none';
-        """
-        driver.execute_script(hide_element_script)
+    hide_element_script = """
+    var element = document.querySelector('div.onetrust-pc-dark-filter.ot-fade-in');
+    if (element) element.style.display = 'none';
+    """
+    driver.execute_script(hide_element_script)
 
-        submit_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-default.log-page__btn-log[type="submit"]')))
-        submit_button.click()
+    submit_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[name="login"]')))
+    submit_button.click()
 
-        track_urls = []
+    time.sleep(5)
 
-        try:
-            playlist_info = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "p.header-page__playlist-tracks")))
-            playlist_info = playlist_info.get_attribute("innerHTML")
-            print("Logged in!")
-        except TimeoutException:
-            print("Error getting playlist information, login failed?")
-            sys.exit(0)
+    track_urls = []
 
-        tracks_total = int(re.search(r"\b\d+\b", playlist_info).group())
+    try:
+        playlist_info = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "p.header-page__playlist-tracks")))
+        playlist_info = playlist_info.get_attribute("innerHTML")
+        print("Logged in!")
+    except TimeoutException:
+        print("Error getting playlist information, login failed?")
+        sys.exit(0)
 
-        draggable_items = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.playlist-tracks-list li.draggable-item")))
+    tracks_total = int(re.search(r"\b\d+\b", playlist_info).group())
+
+    draggable_items = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.playlist-tracks-list li.draggable-item")))
+
+    for draggable_item in draggable_items:
+        grab_track_from_element(draggable_item, track_urls)
+        print("Fetching {:^3} / {:^3}".format(len(track_urls), tracks_total), end="\r", flush=True)
+
+    previous_last_urls = []
+    repeated_urls_counter = 0
+    while len(track_urls) < tracks_total:
+        current_last_urls = []
+        draggable_items = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.playlist-tracks-list li.draggable-item")))[-3:]
 
         for draggable_item in draggable_items:
-            grab_track_from_element(draggable_item, track_urls)
-            print("Fetching {:^3} / {:^3}".format(len(track_urls), tracks_total), end="\r", flush=True)
+            url = grab_track_from_element(draggable_item, track_urls)
+            current_last_urls.append(url)
+            print("Fetching {:>3} / {:<3}".format(len(track_urls), tracks_total), end="\r", flush=True)
 
-        previous_last_urls = []
-        repeated_urls_counter = 0
-        while len(track_urls) < tracks_total:
-            current_last_urls = []
-            draggable_items = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.playlist-tracks-list li.draggable-item")))[-3:]
+        if current_last_urls == previous_last_urls:
+            repeated_urls_counter += 1
 
-            for draggable_item in draggable_items:
-                url = grab_track_from_element(draggable_item, track_urls)
-                current_last_urls.append(url)
-                print("Fetching {:>3} / {:<3}".format(len(track_urls), tracks_total), end="\r", flush=True)
+        if repeated_urls_counter == 3:
+            print(f"\nFound {tracks_total - len(track_urls)} duplicates")
+            break
 
-            if current_last_urls == previous_last_urls:
-                repeated_urls_counter += 1
+        previous_last_urls = current_last_urls
 
-            if repeated_urls_counter == 3:
-                print(f"\nFound {tracks_total - len(track_urls)} duplicates")
-                break
+    driver.quit()
 
-            previous_last_urls = current_last_urls
+    with open("output/tracks.txt", "w") as f:
+        f.write("\n".join(track_urls))
 
-        print("")
-
-        driver.quit()
-
-        with open("output/tracks.txt", "w") as f:
-            f.write("\n".join(track_urls))
-    
-    elif choice == "2":
-
-        track_urls = []
-        with open("output/tracks.txt", "r") as f:
-            track_urls = f.readlines()
-    
-        tracks_total = len(track_urls)
-
-        # Download using doubledouble.top
-        driver.get("https://doubledouble.top/")
-        
-        options_el = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.dl-form > details:nth-child(4)')))
-        options_el.click()
-        hide_download_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#private')))
-        hide_download_input.click()
-        orpheusdl_el = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.dl-form > details:nth-child(4) > div:nth-child(8) > details:nth-child(2)')))
-        orpheusdl_el.click()
-        lyrics_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#lyrics')))
-        lyrics_input.click()
-
-        dl_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#dl-input')))
-        
-        download_counter = 0
-        downloads_failed = []
-
-        for url in track_urls:
-            download_counter += 1
-            print("Downloading {:>3} / {:<3}".format(download_counter, tracks_total), end="\r", flush=True)
-
-            # Wait for the element to be enabled, from previous download iteration
-            start_time = time.time()
-            timeout = 120
-            url_downloaded = False
-            while time.time() - start_time < timeout:
-                if not dl_input.get_attribute("disabled"):
-                    dl_input.clear()
-                    dl_input.send_keys(url)
-                    dl_input.send_keys(Keys.RETURN)
-                    url_downloaded = True
-                    break
-                else:
-                    time.sleep(2)
-            
-            if not url_downloaded:
-                downloads_failed.append(url)
-
-        if downloads_failed:
-            print("Downloading failed for these urls:")
-            print("\n".join(downloads_failed))
+    print("Urls saved!")
 
 if __name__ == "__main__":
     main()
